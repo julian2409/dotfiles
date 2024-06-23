913a014934150d1f882a848dc033a493fa3b7cd7
Julian Schallenmueller <29258884+julian2409@users.noreply.github.com>
2024-06-23 03:50:16 +0200

    Add neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,7 @@
+LANG=en_US
+
+require("lazynvim")
+require("lsp")
+require("remap")
+require("colorscheme")
+require("set")
---
+++ .config/nvim/lua/lsp.lua
@@ -0,0 +1,30 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
+
+require('mason-lspconfig').setup({
+    ensure_installed = {
+        "gopls",
+        "tsserver",
+        "rust_analyzer",
+    },
+    handlers = {
+        function(gopls)
+            require('lspconfig')[gopls].setup({})
+        end,
+    },
+})
