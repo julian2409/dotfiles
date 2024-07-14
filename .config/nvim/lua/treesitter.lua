53a9eded2544aef6a6174b183d46ff130ba1eff0
Julian Schallenmueller <29258884+julian2409@users.noreply.github.com>
2024-07-14 20:47:56 +0200

    Terraform + Docker Language servers

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -14,6 +14,9 @@ require('nvim-treesitter.configs').setup {
         "css",
         "scss",
         "html",
+        "dockerfile",
+        "yaml",
+        "bash",
     },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
