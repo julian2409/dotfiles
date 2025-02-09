f940d7084ccc336f4d334fc868eb3c94e2063fe8
Julian Schallenmueller <29258884+julian2409@users.noreply.github.com>
2025-02-09 21:43:22 +0100

    Fix treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -21,13 +21,14 @@ return {
                 "yaml",
                 "bash",
                 "python",
-            }
+            },
+            sync_install = false,
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+
         })
     end,
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
 }
