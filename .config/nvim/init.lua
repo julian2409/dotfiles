917de7e779321e745fa15ac53d310be7d2205293
Julian Schallenmueller <jsa@novatec-gmbh.de>
2025-03-09 22:23:21 +0100

    More nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,1 @@
-require("lazynvim")
-require("lsp")
-require("remap")
-require("colorscheme")
-require("set")
-require("telescope")
-require("treesitter")
-require("autotag")
-require("surround")
-require('statusline')
-
--- vim.cmd("language en_US")
+require("config.lazy")
